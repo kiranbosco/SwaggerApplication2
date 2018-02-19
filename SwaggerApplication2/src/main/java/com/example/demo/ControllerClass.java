a17f8f5713be411c40b504ce5e1d9f040ea6c6df
Superman computers <Superman computers@DESKTOP-AGHAT19>
2018-02-19 14:47:04 +0530

    new Class added

---
+++ SwaggerApplication2/src/main/java/com/example/demo/ControllerClass.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+
+	@RequestMapping(value="/getmethdoInfo",method=RequestMethod.GET)
+	public String demo() {
+		return "success";
+
+	}
+
+	@RequestMapping(value="/postMethod",method=RequestMethod.POST)
+	public String postDemo(@RequestBody Demo demo) {
+		
+		System.out.println("post api success : " + demo.getDemo_id ());
+		System.out.println("post api success  :  " + demo.getDemo_name());
+		return "post Api Success";
+		
+	}
+	
+	
+	
+}
